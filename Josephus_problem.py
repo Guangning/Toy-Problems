14fef8cf1a15836374403ec0857518f41bf01c19
Guangning Yu <yuguangning@gmail.com>
2014-11-29 23:48:39 +0800

    add Jesophus problem

--- Josephus_problem.py
+++ Josephus_problem.py
@@ -12,61 +12,48 @@
 # to be used for counting. It will return the name of the last person remaining
 # after repetitive counting by num.
 
-class DeathQueue:
+class DeathCircle:
 
     # This is a actually a QUEUE.
 
-    def __init__(self):
-        self.items = []
+    def __init__(self, name_list):
+        self.persons = name_list
 
-    def is_all_dead(self):
-        return self.items == []
+    def next_one(self):
+        self.persons.insert(0, self.persons.pop())
 
-    def enqueue(self, item):
-        self.items.insert(0, item)
+    def kill_him(self):
+        print '%s died.' % self.persons.pop()
 
-    def dequeue(self):
-        return self.items.pop()
+    def how_many_left(self):
+        return len(self.persons)
 
-    def size(self):
-        return len(self.items)
+    def show_current_circle(self):
+        print 'The current circle is: %s.' % self.persons
 
-    def display(self):
-        return self.items
+    def who_is_the_lucky_bastard(self, magic_number):
+        while self.how_many_left() > 1:
+            for i in range(magic_number - 1):
+                self.next_one()
+            self.kill_him()
+        print 'The lucky bastard is %s.' % self.persons[0]
 
-def get_the_last_man(names, magic_number):
-
-    death_circle = Queue()
-    name_list = names.replace(' ', '').split(',')
-
-
-    for name in name_list:
-        death_circle.enqueue(name)
-
-#    while death_circle.size() > 1:
-    for i in range(magic_number):
-        death_circle.enqueue(death_circle.dequeue())
-    print death_circle.display()
-    for i in range(magic_number):
-        death_circle.enqueue(death_circle.dequeue())
-    print death_circle.display()
-
-    for i in range(magic_number):
-        death_circle.enqueue(death_circle.dequeue())
-    print death_circle.display()
 def main():
 
-#    print "Please enter the name list, such as 'Batel, Asaf, Chaya, Dekel, Galia, Herut'."
-#    names = input('Your name list is: ')
-#    print ""
-#
-#    print "Please enter the magic number, such as '3'."
-#    magic_number = input('Your magic number is: ')
-#    print ""
+    print "Please enter the name list, such as 'Batel, Asaf, Chaya, Dekel, Galia, Herut'."
+    names = input('Your name list is: ')
+    print ""
 
-    names = 'a, b, c, d, e, f, g, h, i, j, k, l, m, n'
-    magic_number = 3
-    get_the_last_man(names, magic_number)
+    print "Please enter the magic number, such as '3'."
+    magic_number = input('Your magic number is: ')
+    print ""
+
+    name_list = names.replace(' ', '').split(',')
+    name_list.reverse()
+    death_circle = DeathCircle(name_list)
+
+    death_circle.show_current_circle()
+    death_circle.who_is_the_lucky_bastard(magic_number)
 
 if __name__ == '__main__':
     main()
