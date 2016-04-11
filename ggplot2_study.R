fb4bfbe9b60144edcdb0c3000990b0112d94c927
Guangning Yu <hi@guangningyu.com>
2016-04-11 15:40:12 +0800

    Update ggplot2_study.R

--- ggplot2_study.R
+++ ggplot2_study.R
@@ -1,6 +1,24 @@
 library("ggplot2")
 
-# 1.bar chart
+# 1.line
+
+# 1) use geom_line() to connect dots
+ggplot(economics, aes(date, unemploy)) + geom_line()
+ggplot(economics_long, aes(date, value01, colour=variable)) + geom_line()
+
+# 2) use geom_step() to highlight the y value changes
+recent <- economics[economics$date>as.Date("2013-01-01"),]
+ggplot(recent, aes(date, unemploy)) + geom_step()
+
+# 3) use geom_path() to explore how two variables are related over time
+ggplot(economics, aes(unemploy/pop, psavert)) + geom_path(aes(colour=as.numeric(date)))
+
+# 4) Change parameters
+# change the line colour and add an arrow
+ggplot(economics, aes(date, unemploy)) + geom_line(colour="red", arrow=arrow(angle=15, ends="both", type="closed"))
+
+
+# 2.bar chart
 
 # 1) Number of cars in each class
 ggplot(mpg, aes(class)) + geom_bar()
