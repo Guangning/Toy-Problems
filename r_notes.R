63a700fcabab096c3febc7281f6746edb02e7b28
Guangning Yu <hi@guangningyu.com>
2016-04-14 15:45:12 +0800

    Update r_notes.R

--- r_notes.R
+++ r_notes.R
@@ -2,5 +2,6 @@
 start.date <- as.Date("2015-01-01")
 dates <- seq(from=start.date, by=1, length.out=90)
 
-# 查看数据框的行数和列数
-dim(data)
+nrow(data) # 查看数据框的行数
+ncol(data) # 查看数据框的列数
+dim(data)  # 查看数据框的行数和列数
